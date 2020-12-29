9fe7f8cb26d121966360e38752dfcd4299af9d40
Vitor Argus <vitor.argus@outlook.com>
2020-12-29 16:30:46 -0300

    Upload de arquivos iniciais

---
+++ argus-agenda-entidades/src/br/argus/agenda/entidades/Contato.java
@@ -0,0 +1,33 @@
+package br.argus.agenda.entidades;
+
+public class Contato {
+
+	private String nome;
+	private int idade;
+	private int telefone;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public int getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(int telefone) {
+		this.telefone = telefone;
+	}
+
+}
